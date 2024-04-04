0555cde639876d12e37c3f1bb8f89818ab31d920
KarabasBurak <117377125+KarabasBurak@users.noreply.github.com>
2024-04-04 17:06:39 +0300

    Add project files.

---
+++ AspNetCoreIdentityApp.Web/Models/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System;
+
+namespace AspNetCoreIdentityApp.Web.Models
+{
+    public class AppDbContext:IdentityDbContext<AppUser,AppRole,string>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options):base(options)
+        {
+            
+        }
+    }
+}
