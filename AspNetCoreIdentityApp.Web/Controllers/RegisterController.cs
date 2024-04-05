7268c749219db7a38b454c4701ccc84d5aaddc22
KarabasBurak <117377125+KarabasBurak@users.noreply.github.com>
2024-04-05 16:17:32 +0300

    Registercontroller, RegisterDto, SignUp.cshtml

---
+++ AspNetCoreIdentityApp.Web/Controllers/RegisterController.cs
@@ -0,0 +1,48 @@
+﻿using AspNetCoreIdentityApp.Web.DTOs;
+using AspNetCoreIdentityApp.Web.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreIdentityApp.Web.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(RegisterDto registerDto)
+        {
+           var result=await _userManager.CreateAsync(new()
+            {
+                UserName = registerDto.UserName,
+                Email = registerDto.Email,
+                PhoneNumber=registerDto.Phone,
+                
+            }, registerDto.Password);
+
+            if(result.Succeeded)
+            {
+                TempData["SuccessMessage"] = "Üyelik kayıt işlemi gerçekleşti";
+                return RedirectToAction(nameof(RegisterController.SignUp));
+            }
+
+            foreach(IdentityError item in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, item.Description);
+            }
+            return View();
+
+            
+        }
+    }
+}
---
+++ AspNetCoreIdentityApp.Web/DTOs/RegisterDto.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreIdentityApp.Web.DTOs
+{
+    public class RegisterDto
+    {
+        public RegisterDto()
+        {
+            
+        }
+        public RegisterDto(string userName, string email, string phone, string password)
+        {
+            UserName = userName;
+            Email = email;
+            Phone = phone;
+            Password = password;
+        }
+
+        [Required(ErrorMessage ="Kullanıcı ismi boş olamaz")]
+        [Display(Name ="Username:")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Email boş olamaz")]
+        [Display(Name = "Email:")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Telefon boş olamaz")]
+        [Display(Name = "Phone:")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Şifre boş olamaz")]
+        [Display(Name = "Password:")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Şifrenizi tekrar giriniz")]
+        [Display(Name = "Confirm Password:")]
+        public string ConfirmPassword { get; set; }
+    }
+}
